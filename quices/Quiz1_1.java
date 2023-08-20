509568651cd1c9dd2ab577f86d2bcf21772c15f8
Cristian0121 <141368306+Cristian0121@users.noreply.github.com>
2023-08-20 15:47:03 -0500

    Quiz1_1.java

---
+++ quices/Quiz1_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Quiz1_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int n = scanner.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
